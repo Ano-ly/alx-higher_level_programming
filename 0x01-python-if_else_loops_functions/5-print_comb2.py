5dbe305dd3396408956aa41bda8e7e507c5f50e2
Ano_ly <amarachiuvere@student.oauife.edu.ng>
2023-11-01 20:01:17 +0100

    Edits file 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for iter in range(0, 100):
-    if iter < 10:
-        print("0", end="")
-    if iter == 99:
-        break
-    print("{}, ".format(iter), end="")
-    iter += 1
+    if iter != 99:
+        print("{:02d}, ".format(iter),  end="")
+    else:
+        print("{}".format(iter))
