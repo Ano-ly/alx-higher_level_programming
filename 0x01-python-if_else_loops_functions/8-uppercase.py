b0ddc526c6d432e27851978b24bcae983783685d
Ano_ly <amarachiuvere@student.oauife.edu.ng>
2023-11-01 04:18:30 +0100

    Adds file 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for letter in str:
+        if ord(letter) >= 97 and ord(letter) <= 122:
+            print("{}".format(chr(ord(letter) - 32)), end="")
+    print("\n")
+
+
+uppercase("best")
