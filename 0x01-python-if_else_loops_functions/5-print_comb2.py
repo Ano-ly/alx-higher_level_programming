aa9db5f0cf236a830f40fe7ebe13faf567f2bef1
Ano_ly <amarachiuvere@student.oauife.edu.ng>
2023-11-01 03:59:23 +0100

    Edits file 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,7 +3,6 @@ for iter in range(0, 100):
     if iter < 10:
         print("0", end="")
     if iter == 99:
-        print("{}".format(iter), end="")
         break
     print("{}, ".format(iter), end="")
     iter += 1
