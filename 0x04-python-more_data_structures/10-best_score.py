d0650f5157597f0cf5bc31738c60119331be9116
Ano_ly <amarachiuvere@student.oauife.edu.ng>
2023-11-11 21:41:26 +0100

    Edits file 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if not a_dictionary or a_dictionary is None:
         return None
     dic_max = 0
     max_key = ""
