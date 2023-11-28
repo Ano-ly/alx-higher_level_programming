4a8743860e8714f3313639dc8bb17eb21f12bac6
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2023-11-28 05:36:33 +0100

    Added documentation to files

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 class Rectangle:
+    '''
+    This is a rectangle
+    '''
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
