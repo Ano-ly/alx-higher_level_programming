e2a10265c80b6a8db12f791b9525015d5feb4aaf
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2023-12-05 21:34:53 +0100

    Edit file 6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
-"""This is an empty class"""
+"""This contains one class"""
 
 
 class BaseGeometry:
+    """This is an empty class"""
+
     def area(self):
         """ Raises an exception"""
 
