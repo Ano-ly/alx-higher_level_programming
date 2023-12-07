e6083ffa2333273eeaaf67c811cbdbb6a21e8cf1
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2023-12-07 05:08:18 +0100

    Create new directory, first files

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This contains a function that prints a textfile"""
+
+
+def read_file(filename=""):
+    """
+    This function reads a file
+
+    Args:
+        filename: name of file to be read
+    """
+
+    with open(filename, "r") as file:
+        readd = file.read()
+        print(readd)
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+This project deals with writing and reading from files
