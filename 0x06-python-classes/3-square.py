c98990897da8fbdc51e540d6f4806672ff17e9ab
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2023-12-26 05:23:32 +0100

    Edit file 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
-"""python3 - c 'print(__import__("my_module").__doc__)'
-"""
+"""This document contains a class Square"""
 
 
 class Square:
-    """python3 - c 'print(__import__("my_module").MyClass.__doc__)'
-    """
-    pass
+    """This is a class Square"""
+    def __init__(self, size=0):
+        """
+        This function initialises the square
+
+        Args -
+            size: size to be initialised with
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """This function computes the area of the square"""
+
+        return (self.__size ** 2)
