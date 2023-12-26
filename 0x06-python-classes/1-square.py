9341051a522e8709af02f89ea8297ffd6ff4b30e
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2023-12-26 05:14:51 +0100

    Edit file 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-"""python3 - c 'print(__import__("my_module").__doc__)'
-"""
+"""This document contains a class Square"""
 
 
 class Square:
-    """python3 - c 'print(__import__("my_module").MyClass.__doc__)'
-    """
-    pass
+    """This is a class Square"""
+    def __init__(self, size):
+        """
+        This function initialises the square
+
+        Args -
+            size: size to be initialised with
+        """
+        self.__size = size
