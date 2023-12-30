52b170968c56db39cb4478e6f62a18a6ff06dd71
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2023-12-30 06:44:25 +0100

    Edit file 0 to handle NaN

--- 0x07-python-test_driven_development/test.py
+++ 0x07-python-test_driven_development/test.py
@@ -4,5 +4,4 @@ add_integer = __import__('0-add_integer').add_integer
 print(add_integer(1.3, 2))
 print(add_integer(100, -2))
 print(add_integer(2))
-print(add_integer(1e4566))
-print(add_integer(5.3245675432456765432456754567654324677654))
+print(add_integer(float("nan")))
