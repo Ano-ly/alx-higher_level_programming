d62ef41cf09730641e458194d594abd0a52f9333
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-01-01 20:14:13 +0100

    Edit file 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -29,6 +29,8 @@ integers/floats")
     for row in matrix:
         if len(row) != mat_size:
             raise TypeError("Each row of the matrix must have the same size")
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
     for row in matrix:
