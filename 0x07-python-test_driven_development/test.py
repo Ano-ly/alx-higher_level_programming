6fe3bc4257c79a4f6d5b47eb575f2a2a7848a35b
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-01-01 21:18:11 +0100

    Add file 3

--- 0x07-python-test_driven_development/test.py
+++ 0x07-python-test_driven_development/test.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-matrix_divided = __import__('2-matrix_divided').matrix_divided
+say_my_name = __import__('3-say_my_name').say_my_name
 
-matrix = [
-    [1, 2, 3],
-    [4, 5]
-]
+say_my_name("John", "Smith")
+say_my_name("Walter", "White")
+say_my_name("Bob")
+try:
+    say_my_name(12, "White")
+except Exception as e:
+    print(e)
 
-matrix2 = []
-#print(matrix_divided(matrix, 0))
-#print(matrix_divided(matrix2, 3))
-#print(matrix)
-
-print(matrix_divided([[1, 2, 3], [3, 6, 7]], 4))
