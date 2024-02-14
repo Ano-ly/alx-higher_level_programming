50b199e76f65c025cedf10bf1994853625a87e37
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-02-14 04:53:03 +0100

    Fix some errors and complete task 4

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,5 +8,15 @@ class Rectangle {
     this.width = w;
     this.height = h;
   }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let str = '';
+      for (let j = 0; j < this.width; j++) {
+        str += 'X';
+      }
+      console.log(str);
+    }
+  }
 }
 module.exports = Rectangle;
