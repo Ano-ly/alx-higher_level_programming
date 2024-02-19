40e215193e1497b0d329e0041e02d3e2c12c0f1f
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-02-19 05:04:18 +0100

    First task

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+# This module accesses a database
+
+args = sys.argv
+
+db1 = MySQLdb.connect(host='localhost', port=3306, 
+                      user=args[1], passwd=args[2], db=args[3])
+mycur = db1.cursor()
+
+mycur.execute("SELECT * FROM states ORDER BY states.id ASC")
+intable = mycur.fetchall()
+for row in intable:
+    print("({}: '{}')".format(row[0], row[1]))
