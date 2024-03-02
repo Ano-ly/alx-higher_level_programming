f15bde3e542e18177018412386515263ac77c975
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-03-02 05:48:22 +0100

    complete task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+This module takes in a url, sends a request
+and handles a httperror if it occurs.
+"""
+
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.error
+    import urllib.parse
+    import sys
+
+    args = sys.argv
+    url_sup = args[1]
+
+    message = urllib.request.(url_sup)
+    try:
+        with urllib.request.urlopen(message) as in_resp:
+            print(in_resp.read().decode('utf-8'))
+        except urllib.error.HTTPError as err:
+            print("Error code: {}". format(err.code))
