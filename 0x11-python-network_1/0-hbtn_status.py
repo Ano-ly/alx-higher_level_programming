99980562716a638cac83da15da90c2f012ad2e10
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-03-02 06:01:16 +0100

    gg

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
         resp_read = resp.read()
         print("Body response:")
-        print("\t- type: {}\n\t-\content: {}\n\t- utf8 content: {}".
+        print("\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".
               format(type(resp_read), resp_read, resp_read.decode('utf-8')))
