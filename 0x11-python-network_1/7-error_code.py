8fc13843cd5074387d780b9c602578297454671f
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-03-02 05:56:39 +0100

    Make ghost copies

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This program file fetches https://alx-intranet.hbtn.io/status"""
+
+
+if __name__ == "__main__":
+    import urllib.request
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+        resp_read = resp.read()
+        print("Body response:\n\t- type: {}\n\t-\
+              content: {}\n\t- utf8 content: {}".
+              format(type(resp_read), resp_read, resp_read.decode('utf-8')))
