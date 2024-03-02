8599152985a4c84e11fc642bb2c3cf49d4a6d590
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-03-02 05:38:02 +0100

    add file 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
 
-"""This module takes in a url and email, sends a request and displays the body of the response"""
+"""
+This module takes in a url and email, sends a request 
+and displays the body of the response
+"""
+
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    import sys
+
+    args = sys.argv
+    url_sup = args[1]
+    my_values = {'email': args[2]}
+
+    my_data = urllib.parse.urlencode(my_values)
+    my_data = my_data.encode('ascii')
+    message = urllib.request.Request(url_sup, my_data)
+    with urllib.request.urlopen(message) as in_resp:
+        print(in_resp.decode('utf-8'))
