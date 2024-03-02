3f1506b0c6666df75317834e2bf0272b0ced88a0
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-03-02 05:18:17 +0100

    Complete task 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """This program file fetches https://alx-intranet.hbtn.io/status"""
 
-import urllib.request
 
 if __name__ == "__main__":
+    import urllib.request
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
         resp_read = resp.read()
         print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".
