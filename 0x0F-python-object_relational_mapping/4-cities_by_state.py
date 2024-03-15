adbb91a818656f35a70ed09d564e0496b6f86821
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-03-15 17:03:00 +0100

    Complete task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module accesses a database
+and prints values of columns from two different
+tables using join
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    """Does not execute when imported"""
+
+    args = sys.argv
+
+    db1 = MySQLdb.connect(host='localhost', port=3306,
+                          user=args[1], passwd=args[2], db=args[3])
+    mycur = db1.cursor()
+
+    mycur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                  INNER JOIN states ON cities.state_id =\
+                  states.id ORDER BY cities.id ASC")
+    intable = mycur.fetchall()
+    for row in intable:
+        print("({}, '{}', '{}')".format(row[0], row[1], row[2]))
