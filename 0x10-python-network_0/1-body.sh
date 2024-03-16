45b3af60ce9ae5276cf5790463a8abbfcf87b082
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-03-16 12:49:41 +0100

    Complete task 1

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Send get request and display body if status code is 200
+
+response=$(curl -sw '\n%{http_code}\n' $1)
+
+statuscode=$(echo "${response}" | tail -n 1)
+
+response_length=$(echo "${response}" | wc -l)
+responselen=$((response_length))
+lenn=$((responselen - 1))
+response=$(echo "${response}" | head -n "${lenn}")
+
+if [ "${statuscode}" == "200" ]; then
+	echo "${response}"
+fi
