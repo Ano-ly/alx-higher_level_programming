237f5b33f5f99b1b7c049df555cda628caba3ca7
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-03-16 13:11:39 +0100

    Complete task 100

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Send POSTrequest
-curl -d "email=test@gmail.com" "subject=I will always be here for PLD" "$1"
+curl -s -d "email=test@gmail.com" "subject=I will always be here for PLD" "$1"
