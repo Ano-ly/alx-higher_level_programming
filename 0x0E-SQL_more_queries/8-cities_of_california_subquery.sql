727835d91ef243d0b007547cf15937ec4ebf2d9b
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-03-20 20:48:53 +0100

    Edit file 8 again

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- Select from two tables
 -- Select
 SELECT cities.id, cities.name FROM cities, states WHERE states.name =
-'Carlifornia' ORDER BY cities.id ASC;
+'California' ORDER BY cities.id ASC;
