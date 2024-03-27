1cec696c10fb0c6648d1bee13eff39c78fd98600
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-03-27 05:55:56 +0100

    Complete task 2

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,7 +5,7 @@ const myArgs = process.argv;
 const filename = myArgs[2];
 const content = myArgs[3];
 
-fsMod.writeFile(filename, '\ufeff' + content, (errorV, content) => {
+fsMod.writeFile(filename, content, 'utf-8', (errorV, content) => {
   if (errorV) {
     console.error(errorV);
   }
