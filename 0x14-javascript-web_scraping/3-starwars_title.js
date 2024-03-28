8657e6dd29cc8079762d83a8a7a7b5e12d1cc82e
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-03-28 10:26:16 +0100

    Correct code for task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,19 +2,13 @@
 
 const requestMod = require('request');
 const myArgs = process.argv;
-const url = 'https://swapi-api.alx-tools.com/api/films';
 const idEps = myArgs[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + String(idEps);
 
 requestMod.get(url, function (errorV, response, content) {
   if (errorV) {
     console.error(errorV);
   }
   const myDict = JSON.parse(content);
-  const noEps = myDict.count;
-  const eps = myDict.results;
-  for (let i = 0; i < noEps; i++) {
-    if (eps[i].episode_id === Number(idEps)) {
-      console.log(eps[i].title);
-    }
-  }
+  console.log(myDict.title);
 });
