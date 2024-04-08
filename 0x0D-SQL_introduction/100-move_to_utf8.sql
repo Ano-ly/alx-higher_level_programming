6f70460596ce87f5cc80b14a982037b126d2f724
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-04-08 21:22:32 +0100

    Advanced task

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,7 +3,3 @@
 USE hbtn_0c_0;
 -- convert database
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- convert table
-ALTER TABLE first_table COLLATE utf8mb4_unicode_ci;
--- convert field
-ALTER TABLE first_table MODIFY COLUMN name COLLATE utf8mb4_unicode_ci;
