d846451acabbe56995bae2902fdb1ad6a40ab70a
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-04-08 21:15:33 +0100

    Advanced task

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -6,4 +6,4 @@ ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- convert table
 ALTER TABLE first_table COLLATE utf8mb4_unicode_ci;
 -- convert field
-ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
