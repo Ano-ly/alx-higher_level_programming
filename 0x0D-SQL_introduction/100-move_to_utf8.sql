6e1d350255781c15608abcc0f5ddf37c50418b37
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-04-08 21:30:11 +0100

    Advanced taskk

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,5 +2,5 @@
 -- convert some databases, tables, fields to UTF8
 USE hbtn_0c_0;
 -- convert database
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
