cecec5840d053e931e1e319d59649f82a915dffc
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-04-14 18:41:48 +0100

    Task 7, python ORM

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.sql
@@ -0,0 +1,5 @@
+-- Insert states
+CREATE DATABASE IF NOT EXISTS hbtn_0e_6_usa;
+USE hbtn_0e_6_usa;
+CREATE TABLE IF NOT EXISTS states;
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
