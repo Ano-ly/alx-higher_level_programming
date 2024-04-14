f9943c5f56f151b548452381294b8e38bb248ba9
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-04-14 18:49:53 +0100

    Task 8, python ORM

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Print first state in database"""
+
+if __name__ == '__main__':
+    from model_state import Base, State
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    args = sys.argv
+    user = args[1]
+    pswd = args[2]
+    db = args[3]
+
+    my_engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                              format(user,
+                                     pswd,
+                                     'localhost:3306',
+                                     db))
+    Base.metadata.create_all(bind=my_engine)
+    Session = sessionmaker(my_engine)
+    new_session = Session()
+
+    objs = new_session.query(State).filter(State.id == 1)
+    my_row = objs.first()
+    print("{}: {}".format(my_row.id, my_row.name))
+
+    new_session.close()
