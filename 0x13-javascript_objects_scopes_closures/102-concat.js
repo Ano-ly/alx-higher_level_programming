685ce029466898c019a1e306e4acd564ecaa13dd
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-08-21 15:21:05 +0100

    Task 102

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const args = process.argv;
+
+fs.appendFile(args[4], args[2], function (err) {
+  if (err) {
+    throw err;
+  }
+});
+fs.appendFile(args[4], '\n', function (err) {
+  if (err) {
+    throw err;
+  }
+});
+fs.appendFile(args[4], args[3], function (err) {
+  if (err) {
+    throw err;
+  }
+});
