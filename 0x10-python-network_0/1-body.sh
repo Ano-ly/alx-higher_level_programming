9750b6df758008940e0a34da467ad58b84fb6bb3
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-08-28 05:25:31 +0100

    Task 1 edit

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send get request and display body if status code is 200
-curl -s "$1" -o /dev/null -w "%{http_code}" | grep -q 200 && curl -s "$1"
+curl -sL "$1" -o /dev/null -w "%{http_code}" | grep -q 200 && curl -sL "$1"
